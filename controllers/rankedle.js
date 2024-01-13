ebee17c8ca08c2e521422972e0d88886d75a5f51
Hei5enberg <46035253+Hei5enberg44@users.noreply.github.com>
2024-01-13 14:38:50 +0100

    Update rankedle.js

--- controllers/rankedle.js
+++ controllers/rankedle.js
@@ -887,11 +887,12 @@ export default class Rankedle {
         const globalStats = []
         for(const s of globalStatsData) {
             const user = await members.getUser(s.memberId)
+            if(!user) continue
             globalStats.push({
                 ...s,
                 player: {
-                    name: user ? user.username : s.memberId,
-                    avatar: user ? `${user.getAvatarURL()}?size=80` : ''
+                    name: user.username,
+                    avatar: `${user.getAvatarURL()}?size=80`
                 }
             })
         }
@@ -932,11 +933,12 @@ export default class Rankedle {
                 const seasonStats = []
                 for(const s of seasonStatsData) {
                     const user = await members.getUser(s.memberId)
+                    if(!user) continue
                     seasonStats.push({
                         ...s,
                         player: {
-                            name: user ? user.username : s.memberId,
-                            avatar: user ? `${user.getAvatarURL()}?size=80` : ''
+                            name: user.username,
+                            avatar: `${user.getAvatarURL()}?size=80`
                         }
                     })
                 }
